7597a60f603d9c57a226d835fe07b11516f4c4c4
agupeiretti <agupeiretti@gmail.com>
2023-05-23 11:10:56 -0300

    socket mongoose cart

--- src/dao/mongo/managers/cart.js
+++ src/dao/mongo/managers/cart.js
@@ -1,25 +1,68 @@
 import cartsModel from "../models/carts.js";
+import productModel from "../models/products.js";
 
 export default class CartsManager {
   getCarts = (params) => {
     return cartsModel.find(params).lean();
   };
 
-  getCartsBy = (params) => {
-    return cartsModel.findOne(params).lean();
+  getCartById = (cid) => {
+    return cartModel.findById(cid);
   };
 
   createCart = (product) => {
     return cartsModel.create(product);
   };
 
-  addProductToCart = () => {};
-
-  updateCart = (id, products) => {
-    return cartsModel.findByIdAndUpdate(id, { $set: { products } });
+  deleteCart = (cid) => {
+    return cartsModel.findByIdAndDelete(cid);
   };
 
-  deleteCart = (id) => {
-    return cartsModel.findByIdAndDelete(id);
+  addProductToCart = (cid, pid) => {
+    //verifico si existe el carrito
+    const cart = cartsModel.findById(cid);
+    if (!cart) {
+      console.log("ese carrito no existe");
+    }
+    //si esta el produc sumo uno al quantity
+    const productIsInCart = cart.products.find(
+      (product) => product.product.toString() === pid
+    );
+    if (productIsInCart) {
+      productIsInCart.quantity += 1;
+    } else {
+      cart.products.push({ product: pid });
+    }
+
+    //actualizo el total amount del carrito
+    const product = productModel.findById(pid);
+    cart.totalAmount += product.price;
+
+    //ahora gusrdo todo esto en el carrito con save
+    cart.save();
+    return cart;
+  };
+
+  deleteProductToCart = (cid, pid) => {
+    const cart = cartsModel.findById(cid);
+    if (!cart) {
+      console.log("carrito no encontrado");
+    }
+
+    const productIndex = cart.products.findIndex((p) => {
+      p.product.toString() === pid;
+    });
+    if (productIndex === -1) {
+      console.log("Este Producto no esta en el carrito");
+    }
+    //ahora le resto el precio al totalAmount:
+
+    const product = productModel.findById(pid);
+    cart.totalAmount -= product.price * cart.products[productIndex].quantity;
+    //borro el profucto del carrito:
+    cart.products.splice(productIndex, 1);
+    //guardo los cambios del carrito:
+    cart.save();
+    return cart;
   };
 }
